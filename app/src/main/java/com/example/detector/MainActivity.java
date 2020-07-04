daed757fde89a8f4adee2df30191c1a3bc7839df
prabhat1209 <prabhatbharadwaj33@gmail.com>
2020-07-04 17:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/detector/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.detector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.check);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkEmulator())
+                    Toast.makeText(MainActivity.this,"App is running on Emulator",Toast.LENGTH_LONG).show();
+
+                if(!checkEmulator())
+                    Toast.makeText(MainActivity.this,"App is running on Real Android Device",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    static boolean checkEmulator() {
+
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.PRODUCT.contains("sdk_google")
+                || Build.PRODUCT.contains("google_sdk")
+                || Build.PRODUCT.contains("sdk")
+                || Build.PRODUCT.contains("sdk_x86")
+                || Build.PRODUCT.contains("vbox86p")
+                || Build.PRODUCT.contains("emulator")
+                || Build.PRODUCT.contains("simulator");
+
+    }
+}
